7f41a128e1acdc539624687772b264254e4eeb03
scubaninja <scubaninja>
2004-01-04 20:31:56 +0000

    added backup_mysql.sh

---
+++ utils/backup_mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This is a simple shell script to backup a MySQL database with rsnapshot.
+#
+# The assumption is that this will be invoked from rsnapshot. Also, since it
+# will run unattended, the user that runs rsnapshot (probably root) should have
+# a .my.cnf file in their home directory that contains the password for the
+# MySQL root user.
+#
+# This script simply needs to dump a file into the current working directory.
+# rsnapshot handles everything else.
+
+# backup the database
+/usr/bin/mysqldump --all-databases > mysqldump_all_databases.sql
+
+# make the backup readable only by root
+/bin/chmod 600 mysqldump_all_databases.sql
