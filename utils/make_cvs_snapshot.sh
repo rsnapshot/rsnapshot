5870b1a5e0b32d3ae47fffde244a5594cd2b2ca3
scubaninja <scubaninja>
2004-04-15 07:45:31 +0000

    added simple util to find/replace version number for CVS snapshots

---
+++ utils/make_cvs_snapshot.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# make_cvs_snapshot.sh
+#
+# this script just does a find/replace in the source tree to change
+# the version number to CVS-$DATE
+#
+# this was done before manually, now it's automatic
+
+if [ $PWD = "$HOME/rsnapshot" ]; then
+	echo "This is not where you want to be. cp -r to a different directory first!"
+	echo "Quitting now!"
+	exit 1
+fi
+
+VERSION=`./rsnapshot-program.pl version_only | sed s/\\\./\\\\\\\./g`
+DATE=`date +"%Y%m%d"`
+
+perl -pi -e s/$VERSION/CVS-$DATE/g `find . -type f`
