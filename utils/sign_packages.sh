2114747958d8f9dcb8f41cc84869e83177a04fe8
scubaninja <scubaninja>
2004-04-15 07:59:11 +0000

    added sign_packages.sh

---
+++ utils/sign_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# sign_packages.sh
+#
+# This is the script used to semi-automatically GPG sign rsnapshot releases
+
+for file in `/bin/ls *.tar.gz *.deb *.rpm | grep -v latest`; do
+	# MD5
+	if [ ! -e "$file.md5" ]; then
+		md5sum $file > $file.md5;
+		echo "Created MD5 Hash for $file";
+	fi
+	
+	# PGP
+	if [ ! -e "$file.asc" ]; then
+		gpg --armor --detach-sign $file;
+		echo "Created PGP Signature for $file";
+	fi
+done
