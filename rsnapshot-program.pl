b44f5aea8ce00d62afec2b3b408f159b837849aa
scubaninja <scubaninja>
2004-07-10 09:45:37 +0000

    Clarified man page licensing (GPL)

--- rsnapshot-program.pl
+++ rsnapshot-program.pl
@@ -4266,6 +4266,9 @@ Copyright (C) 2003-2004 Nathan Rosenquist
 Portions Copyright (C) 2002-2003 Mike Rubel, Carl Wilhelm Soderstrom,
 Ted Zlatanov
 
+This man page is distributed under the same license as rsnapshot:
+the GPL (see below).
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
