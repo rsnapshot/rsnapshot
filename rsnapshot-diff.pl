7e5e71e8f30d35896c45829d03d5c45c07737b9b
drhyde <drhyde>
2006-05-31 19:51:03 +0000

    Added docs for rsnapshot-diff

--- rsnapshot-diff.pl
+++ rsnapshot-diff.pl
@@ -13,7 +13,14 @@
 # http://www.rsnapshot.org/
 ##############################################################################
 
-# $Id: rsnapshot-diff.pl,v 1.2 2005/07/24 18:48:58 scubaninja Exp $
+# $Id: rsnapshot-diff.pl,v 1.3 2006/05/31 19:51:03 drhyde Exp $
+
+=head1 NAME
+
+rsnapshot-diff - a utility for comparing the disk usage of two snapshots
+taken by rsnapshot
+
+=cut
 
 use strict;
 
@@ -50,6 +57,59 @@ if ($opts{'h'}) {
 };
 	exit;
 }
+
+=head1 SYNOPSIS
+
+rsnapshot-diff [-h|vVi] dir1 dir2
+
+=head1 DESCRIPTION
+
+rsnapshot-diff is a companion utility for rsnapshot, which traverses two
+parallel directory structures and calculates the difference between them.
+By default it is silent apart from displaying summary information at the
+end, but it can be made more verbose.
+
+In the summary, "added" files may very well include files which at first
+glance also appear at the same place in the older directory structure.
+However, because the files differ in some respect, they are different files.
+They have a different inode number.  Consequently if you use -v most of its
+output may appear to be pairs of files with the same name being removed
+and added.
+
+=head1 OPTIONS
+
+=over 4
+
+=item -h (help)
+
+Displays help information
+
+=item -v (verbose)
+
+Be verbose.  This will spit out a list of all changes as they are encountered,
+as well as the summary at the end.
+
+=item -V (more verbose)
+
+Be more verbose - as well as listed changes, unchanged files will be listed
+too.
+
+=item -i (ignore)
+
+If verbosity is turned on, -i suppresses information about symlinks,
+directories, and special files.
+
+=item dir1 and dir2
+
+These are the only compulsory parameters, and should be the names of two
+directories to compare.  Their order doesn't matter, rsnapshot-diff will
+always compare the younger to the older, so files that appear only in the
+older will be reported as having been removed, and files that appear only
+in the younger will be reported as having been added.
+
+=back
+
+=cut
 
 # verbose
 if ($opts{'v'}) { $verbose = 1; }
@@ -160,3 +220,39 @@ sub remove {
         return @stat;
     }
 }
+
+=head1 SEE ALSO
+
+rsnapshot
+
+=head1 BUGS
+
+Please report bugs (and other comments) to the rsnapshot-discuss mailing list:
+
+L<http://lists.sourceforge.net/lists/listinfo/rsnapshot-discuss>
+
+=head1 AUTHOR
+
+David Cantrell E<lt>david@cantrell.org.ukE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2005 David Cantrell
+
+=head1 LICENCE
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+=cut
