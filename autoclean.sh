9702757fb31e1195514b94c3a6919c74ef5377c6
Benedikt Heine <bebe@bebehei.de>
2015-01-05 08:01:57 +0100

    add autgen.sh and autoclean.sh
    both bash files provide routines to generate and clean scripts
    add rsnapshot.1 rsnapshot-diff.1 as clean-path to Makefile

---
+++ autoclean.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -xu
+
+[ -f Makefile ] && make clean
+rm -rf autom4te.cache
+rm -f {config.h.in,config.h}
+rm -f {Makefile.in,Makefile}
+rm -f config.status
+rm -f configure
+rm -f stamp*
+rm -f aclocal.m4
+rm -f compile
+rm -f missing
+rm -f install-sh
