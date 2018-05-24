15295f1740b733dc9a89741635138046f05f8e29
Sérgio Prata <mephx@users.noreply.github.com>
2018-05-24 20:56:22 +0100

    "ssh_args" does not work due to lack of quoting
    adding the fix to https://github.com/rsnapshot/rsnapshot/issues/208

--- rsnapshot-program.pl
+++ rsnapshot-program.pl
@@ -3734,7 +3734,7 @@ sub rsync_backup_point {
 
 		# if we have any args for SSH, add them
 		if (defined($ssh_args)) {
-			push(@rsync_long_args_stack, "--rsh=$config_vars{'cmd_ssh'} $ssh_args");
+			push(@rsync_long_args_stack, "--rsh=\"$config_vars{'cmd_ssh'} $ssh_args"\");
 
 		}
 
